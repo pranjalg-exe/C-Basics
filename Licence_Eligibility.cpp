87a4089b2a79578a5e2e162c75e291774f6b8440
Pranjal Gupta <pranjal.gupta.1586@gmail.com>
2025-04-18 23:55:35 +0530

    Add files via upload

---
+++ Licence_Eligibility.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int age;
+	cin >> age;
+	if (age>=18){
+	    cout<<"You're eligible to drive";
+	}
+	else if(age<18){
+	    cout<<"You're not eligible to dirve";
+	}
+	return 0;
+
+}
